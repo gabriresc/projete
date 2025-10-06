a60e0da4c97aae49cafebf91d61a16cf13f4ee11
gabriresc <gabriresc@gmail.com>
2025-10-06 08:38:39 -0300

    arruando o visual

--- teste.py
+++ teste.py
@@ -75,18 +75,24 @@ def process():
                 min_y1 = y1
                 tamanho = y2 - y1
                 box_ref = (x1, y1, x2, y2)
-        num = 1 
+
         for box, conf, cls in zip(boxes, confs, classes):
             x1, y1, x2, y2 = map(int, box)
             w = y2 - y1
             verificador = 0
-            if(tamanho>w):
+                # Se for o box de referência, pula
+            if (x1, y1, x2, y2) == box_ref:
+                cv2.rectangle(img_rgb, (x1, y1), (x2, y2), (255, 255, 0), 2)
+                cv2.putText(img_rgb, "Referencia", (x1, y1 - 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
+
+            elif(tamanho>w):
                 verificador = 1
                 # Desenha retângulo
                 cv2.rectangle(img_rgb, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
                 # Escreve  classe + confiança
-                label = f"{'Correto'} {conf:.2f} {num}"
+                label = f"{'Correto'}"
                 cv2.putText(img_rgb, label, (x1, y1 - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
             else:
@@ -94,7 +100,7 @@ def process():
                 cv2.rectangle(img_rgb, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
                 # Escreve classe + confiança
-                label = f"{'Incorreto'} {conf:.2f} {num}"
+                label = f"{'Incorreto'}"
                 cv2.putText(img_rgb, label, (x1, y1 - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
             boxes_all.append({
@@ -104,13 +110,8 @@ def process():
                 "y2": y2,
                 'veri':verificador
             })
-            num += 1
-    
-    if box_ref:
-        x1, y1, x2, y2 = box_ref
-        cv2.rectangle(img_rgb, (x1, y1), (x2, y2), (255, 255, 0), 2)
-        cv2.putText(img_rgb, "Referencia", (x1, y1 - 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
+
+
 
     cv2.imshow("Deteccao", img_rgb)
     cv2.waitKey(0)
